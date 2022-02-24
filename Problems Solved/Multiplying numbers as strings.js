718c67793de047a34a6646f1674e0359b03d239e
BalouliYacine <cjvfyv@gmail.com>
2022-02-24 12:24:33 +0100

    PS 1

---
+++ Problems Solved/Multiplying numbers as strings.js
@@ -0,0 +1,10 @@
+console.log("------- ON --------");
+
+function multiply(a, b) {
+    return `${(BigInt(a) * BigInt(b))}`
+}
+
+console.log(multiply("17667", "6956758760056565658568"));
+
+
+console.log("------- OFF --------");
