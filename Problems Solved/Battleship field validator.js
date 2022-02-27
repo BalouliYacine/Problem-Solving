b31fb206ef2b56b88124198c76c37077b3f733ed
BalouliYacine <cjvfyv@gmail.com>
2022-02-27 18:07:06 +0100

    Exempels 1.2

--- Problems Solved/Battleship field validator.js
+++ Problems Solved/Battleship field validator.js
@@ -1,19 +1,79 @@
 console.log("------- ON --------");
-// function validateBattlefield(field) {
-//     for (let I = 0; I < field.length; I++) {
-//         console.log(field[I]);
-//     }
-// }
 
-// validateBattlefield([
-//     [1, 0, 0, 0, 0],
-//     [1, 0, 1, 0, 0],
-//     [1, 0, 1, 0, 1],
-//     [1, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0],
-// ])
+function validateBattlefield(field) {
+    let shipsCount = 0
+    let shipsType = []
+    let check = true
+    //
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {
+            if (field[i][j] === 1) {
+                field[i][j] = '#'
+                // H Check
+                if (field[i][j + 1] === 1) {
+                    field[i][j + 1] = '§'
+                    if (field[i + 1][j + 1]) return check = false
+                    if (field[i - 1] != undefined
+                        && field[i - 1][j + 1]) return check = false
+                    //
+                    if (
+                        field[i][j + 2] === 1) {
+                        field[i][j + 2] = '§'
+                        if (field[i + 1][j + 2]) return check = false
+                        if (field[i - 1] != undefined
+                            && field[i - 1][j + 2]) return check = false
+                        //
+                        if (field[i][j + 3] === 1) {
+                            field[i][j + 3] = '§'
+                            if (field[i + 1][j + 3]) return check = false
+                            if (field[i - 1] != undefined
+                                && field[i - 1][j + 3]) return check = false
+                        }
+                    }
+                }
+                // V Check
+                if (field[i + 1] != undefined
+                    && field[i + 1][j] === 1) {
+                    field[i + 1][j] = '§'
+                    if (field[i + 1][j + 1]) return check = false
+                    if (field[i + 1][j - 1]) return check = false
+                    //
+                    if (field[i + 2] != undefined
+                        && field[i + 2][j] === 1) {
+                        field[i + 2][j] = '§'
+                        if (field[i + 2][j + 1]) return check = false
+                        if (field[i + 2][j - 1]) return check = false
+                        //
+                        if (field[i + 3] != undefined
+                            && field[i + 3][j] === 1) {
+                            field[i + 3][j] = '§'
+                            if (field[i + 3][j + 1]) return check = false
+                            if (field[i + 3][j - 1]) return check = false
+                        }
+                    }
+                }
+                shipsCount++
+            }
+        }
+    }
+    return check
+}
 
+console.log(
+    validateBattlefield([
+        [1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+        [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+        [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+);
 
+console.log("------- OFF --------");
 
 
 
@@ -43,8 +103,7 @@ console.log("------- ON --------");
 //     if (n === 0) return 1
 //     if (m === 0 || n < 0) return 0
 //     else {
-//         return Rec3(n - m, m) + Rec3(n, m - 1)
-//     }
+//         return Rec3(n - m, m) + Rec3(n, m - 1) }
 // }
 
 
@@ -55,4 +114,3 @@ console.log("------- ON --------");
 
 
 
-console.log("------- OFF --------");
