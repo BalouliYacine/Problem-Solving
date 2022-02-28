5e7cc937a19dfa855abd940af6173c9785425035
BalouliYacine <cjvfyv@gmail.com>
2022-02-28 14:37:35 +0100

    Adding task

--- Problems Solved/Battleship field validator.js
+++ Problems Solved/Battleship field validator.js
@@ -1,63 +1,148 @@
 console.log("------- ON --------");
 
 function validateBattlefield(field) {
-    let shipsCount = 0
     let shipsType = []
-    let check = true
+    let counter = 0
     //
     for (let i = 0; i < field.length; i++) {
         for (let j = 0; j < field[i].length; j++) {
             if (field[i][j] === 1) {
-                field[i][j] = '#'
-                // H Check
-                if (field[i][j + 1] === 1) {
-                    field[i][j + 1] = '§'
-                    if (field[i + 1][j + 1]) return check = false
-                    if (field[i - 1] != undefined
-                        && field[i - 1][j + 1]) return check = false
-                    //
-                    if (
-                        field[i][j + 2] === 1) {
-                        field[i][j + 2] = '§'
-                        if (field[i + 1][j + 2]) return check = false
-                        if (field[i - 1] != undefined
-                            && field[i - 1][j + 2]) return check = false
-                        //
-                        if (field[i][j + 3] === 1) {
-                            field[i][j + 3] = '§'
-                            if (field[i + 1][j + 3]) return check = false
-                            if (field[i - 1] != undefined
-                                && field[i - 1][j + 3]) return check = false
-                        }
+                field[i][j] = '§'
+                shipsType.push(1)
+                let p = 1
+                function HCheck(a, b) {
+                    if (p === 4) return
+                    else if (field[a][b + p] === 1) {
+                        field[a][b + p] = "§"
+                        shipsType.pop()
+                        shipsType.push(p + 1)
+                        p++
+                        HCheck(a, b)
                     }
                 }
-                // V Check
-                if (field[i + 1] != undefined
-                    && field[i + 1][j] === 1) {
-                    field[i + 1][j] = '§'
-                    if (field[i + 1][j + 1]) return check = false
-                    if (field[i + 1][j - 1]) return check = false
-                    //
-                    if (field[i + 2] != undefined
-                        && field[i + 2][j] === 1) {
-                        field[i + 2][j] = '§'
-                        if (field[i + 2][j + 1]) return check = false
-                        if (field[i + 2][j - 1]) return check = false
-                        //
-                        if (field[i + 3] != undefined
-                            && field[i + 3][j] === 1) {
-                            field[i + 3][j] = '§'
-                            if (field[i + 3][j + 1]) return check = false
-                            if (field[i + 3][j - 1]) return check = false
-                        }
+                //
+                function VCheck(a, b) {
+                    if (p === 4) return
+                    else if (field[a + p] != undefined
+                        && field[a + p][b] === 1) {
+                        field[a + p][b] = '§'
+                        shipsType.pop()
+                        shipsType.push(p + 1)
+                        p++
+                        VCheck(a, b)
                     }
                 }
-                shipsCount++
+                HCheck(i, j)
+                p = 1
+                VCheck(i, j)
             }
         }
     }
-    return check
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {
+            if (field[i][j] === "§") {
+                if (field[i + 1][j] === "§") counter++
+                if (i != 0 && field[i - 1][j] === "§") counter++
+                if (field[i][j + 1] === "§") counter++
+                if (field[i][j - 1] === "§") counter++
+                if (i != 0 && field[i - 1][j - 1] === "§") counter++
+                if (field[i + 1][j + 1] === "§") counter++
+                if (i != 0 && field[i - 1][j + 1] === "§") counter++
+                if (field[i + 1][j - 1] === "§") counter++
+            }
+        }
+    }
+    function arraysEqual(a, b) {
+        if (a === b) return true;
+        if (a == null || b == null) return false;
+        if (a.length !== b.length) return false;
+        for (var i = 0; i < a.length; ++i) {
+            if (a[i] !== b[i]) return false;
+        }
+        return true;
+    }
+    return counter === 20 && arraysEqual(shipsType.sort(), [1, 1, 1, 1, 2, 2, 2, 3, 3, 4])
 }
+
+// solution 2
+// function validateBattlefield(field) {
+//     let shipsType = []
+//     let counter = 0
+//     // vertical and horizontal chack
+//     for (let i = 0; i < field.length; i++) {
+//         for (let j = 0; j < field[i].length; j++) {
+//             if (field[i][j] === 1) {
+//                 field[i][j] = '§'
+//                 shipsType.push(1)
+//                 // H Check  
+//                 if (field[i][j + 1] === 1) {
+//                     field[i][j + 1] = '§'
+//                     shipsType.pop()
+//                     shipsType.push(2)
+//                     if (
+//                         field[i][j + 2] === 1) {
+//                         field[i][j + 2] = '§'
+//                         shipsType.pop()
+//                         shipsType.push(3)
+//                         if (field[i][j + 3] === 1) {
+//                             field[i][j + 3] = '§'
+//                             shipsType.pop()
+//                             shipsType.push(4)
+//                         }
+//                     }
+//                 }
+//                 // V Check
+//                 else if (field[i + 1] != undefined
+//                     && field[i + 1][j] === 1) {
+//                     field[i + 1][j] = '§'
+//                     shipsType.pop()
+//                     shipsType.push(2)
+//                     if (field[i + 2] != undefined
+//                         && field[i + 2][j] === 1) {
+//                         field[i + 2][j] = '§'
+//                         shipsType.pop()
+//                         shipsType.push(3)
+//                         if (field[i + 3] != undefined
+//                             && field[i + 3][j] === 1) {
+//                             field[i + 3][j] = '§'
+//                             shipsType.pop()
+//                             shipsType.push(4)
+//                         }
+//                     }
+//                 }
+//             }
+
+//         }
+//     }
+//     // corner check
+//     for (let i = 0; i < field.length; i++) {
+//         for (let j = 0; j < field[i].length; j++) {
+//             if (field[i][j] === "§") {
+//                 if (field[i + 1][j] === "§") counter++
+//                 if (i != 0 && field[i - 1][j] === "§") counter++
+//                 if (field[i][j + 1] === "§") counter++
+//                 if (field[i][j - 1] === "§") counter++
+//                 if (i != 0 && field[i - 1][j - 1] === "§") counter++
+//                 if (field[i + 1][j + 1] === "§") counter++
+//                 if (i != 0 && field[i - 1][j + 1] === "§") counter++
+//                 if (field[i + 1][j - 1] === "§") counter++
+//             }
+//         }
+//     }
+//     // valid chip number and type check
+//     function arraysEqual(a, b) {
+//         if (a === b) return true;
+//         if (a == null || b == null) return false;
+//         if (a.length !== b.length) return false;
+//         for (var i = 0; i < a.length; ++i) {
+//             if (a[i] !== b[i]) return false;
+//         }
+//         return true;
+//     }
+
+//     return counter === 20 &&
+//         arraysEqual(shipsType.sort(), [1, 1, 1, 1, 2, 2, 2, 3, 3, 4])
+// }
 
 console.log(
     validateBattlefield([
@@ -65,52 +150,14 @@ console.log(
         [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
         [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
         [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 );
 
 console.log("------- OFF --------");
 
-
-
-// Recursive
-
-// Exemple 1
-// Rrc(n) = Rec(n-1) + n
-// function Rec(n) {
-//     if (n === 0) return 0
-//     else {
-//         return n + Rec(n - 1)
-//     }
-// }
-// console.log(Rec(5));
-
-// Exemple 2
-// function Rec2(n, m) {
-//     if (n === 1 || m === 1) return 1
-//     else {
-//         return Rec2(n - 1, m) + Rec2(n, m - 1)
-//     }
-// }
-// console.log(Rec2(4, 5));
-
-// Exemple 3
-// function Rec3(n, m) {
-//     if (n === 0) return 1
-//     if (m === 0 || n < 0) return 0
-//     else {
-//         return Rec3(n - m, m) + Rec3(n, m - 1) }
-// }
-
-
-// console.log(Rec3(9, 5));
-
-
-
-
-
-
+// https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/javascript
